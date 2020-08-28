1716df2a2dd6878a96f07327f8a715a0d9060759
Zé Yuri <jozeyuri@gmail.com>
2020-08-28 11:18:14 -0300

    initial configs

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ["../stories/*.stories.@(ts|tsx|js|jsx|mdx)"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-links"],
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { ChakraProvider, CSSReset } from "@chakra-ui/core"
+import theme from "@chakra-ui/theme"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
